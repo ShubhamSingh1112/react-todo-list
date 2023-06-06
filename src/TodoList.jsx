43d8c60c3747b21ed1611a59d7831d3d5d9aa8b8
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-06-06 23:34:02 +0530

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,18 @@
+import TodoItem from "./TodoItem";
+
+// eslint-disable-next-line react/prop-types
+export default function NewTodoForm({ todos, toggleTodo, deleteTodo }) {
+
+  return (
+    <ul className="list">
+      {/* eslint-disable-next-line react/prop-types */}
+      {todos.length === 0 && "No Todos"}
+      {/* eslint-disable-next-line react/prop-types */}
+      {todos.map(todo => {
+        return (
+          <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        )
+      })}
+    </ul>
+  )
+}
